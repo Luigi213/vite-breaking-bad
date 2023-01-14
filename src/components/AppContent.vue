cd5088bd34740ddf457fb4cf85e1e9fa4fe7e440
Luigi213 <reyesgianluigi88@gmail.com>
2023-01-14 13:07:03 +0100

    add the styles of loader

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,13 +1,23 @@
 <script>
 import Cardyugi from './Cardyugi.vue';
+import { store } from '../store.js'
 export default {
     components: {
         Cardyugi
+    },
+    data(){
+        return{
+            store
+        }
     }
 }   
 </script>
 <template lang="">  
-    <div class="bg">
+    <div v-if="store.cardList =[]" class="disp">
+        <h1>caricamento in corso</h1>
+        <div class="loader"></div>
+    </div>
+    <div v-else class="bg">
         <div class="container">
             <div class="row">
                 <h3>Found 39 cards</h3>
@@ -18,9 +28,29 @@ export default {
 </template>
 <style lang="scss" scoped>
     @use '../style/partials/variables' as *;
+    .disp{
+        height: 100px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        h1{
+            margin-right: 20px;
+        }
+        .loader {
+            border: 10px solid #f3f3f3; /* Light grey */
+            border-top: 10px solid #3498db; /* Blue */
+            border-radius: 50%;
+            width: 50px;
+            height: 50px;
+            animation: spin 1s linear infinite;
+        }
+        @keyframes spin {
+            0% { transform: rotate(0deg); }
+            100% { transform: rotate(360deg); }
+        }
+    }
     .bg{
         background-color: orange ;
-
         .container{
             .row{
                 display: flex;
--- src/store.js
+++ src/store.js
@@ -4,5 +4,4 @@ export const store = reactive({
     url: 'https://db.ygoprodeck.com/api/v7/cardinfo.php',
     cardList: [],
     loanding: true,
-    cardActive: 0
 })
